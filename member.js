2ac8897e29cd2ca440ddb4dceb2684008da6ded8
TJ <75922615+Tanush-J@users.noreply.github.com>
2024-01-04 04:05:24 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,13 @@
+function skillsMemeber() {
+    var skills = ["HTML", "CSS", "JS", "React", "Node"];
+    var member = {
+        name: "John",
+        age: 20,
+        skills: skills,
+    };
+    console.log(member.skills);
+    // console.log(member.skills[1]);
+    // console.log(member.skills[4]);
+    // console.log(member.skills[5]);
+    // console.log(member.skills.length);
+}
